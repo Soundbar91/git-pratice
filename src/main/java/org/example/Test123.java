c3a714fd921a6d55f13d2d91f48f22d2151cfec8
신관규 <soundbar91@gmail.com>
2024-09-01 13:32:23 +0900

    1:32

--- src/main/java/org/example/Test123.java
+++ src/main/java/org/example/Test123.java
@@ -4,5 +4,6 @@ public class Test123 {
     public static void main(String[] args) {
         System.out.println(123123);
         System.out.println("1:31");
+        System.out.println("1:32");
     }
 }
