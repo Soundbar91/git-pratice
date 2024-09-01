5e8a33b86e564b9c8ec938d34f5c2562ab30899e
신관규 <soundbar91@gmail.com>
2024-09-01 13:56:53 +0900

    1:57

--- src/main/java/org/example/Test12345.java
+++ src/main/java/org/example/Test12345.java
@@ -4,5 +4,6 @@ public class Test12345 {
     public static void main(String[] args) {
         System.out.println("1:56");
         System.out.println("1:58");
+        System.out.println("1:57");
     }
 }
