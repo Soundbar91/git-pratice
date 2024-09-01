8ced8dd8faec930693b1d9888d1f66cd2c394cc9
신관규 <soundbar91@gmail.com>
2024-09-01 13:35:44 +0900

    squash A

--- src/main/java/org/example/Test123.java
+++ src/main/java/org/example/Test123.java
@@ -3,5 +3,7 @@ package org.example;
 public class Test123 {
     public static void main(String[] args) {
         System.out.println(123123);
+        System.out.println("1:31");
+        System.out.println("1:32");
     }
 }
